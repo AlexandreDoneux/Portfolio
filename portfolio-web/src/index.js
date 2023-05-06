8bb4623f8d0697c02ea80371493f77d84513dbf2
Alexandre Doneux <he201962@students.ephec.be>
2023-05-07 00:14:59 +0200

    [refactor] Moving Page Router to index.js and adding upper menu base

--- portfolio-web/src/index.js
+++ portfolio-web/src/index.js
@@ -2,14 +2,82 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import MenuBar from './components/BaseStructure';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+import Cv from './components/Cv';
+import Portfolio from './components/Portfolio';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+
+
+import {ThemeProvider, createTheme } from '@mui/material/styles';
+import { unstable_styleFunctionSx, styled } from '@mui/system';
+
+const Div = styled('div')(unstable_styleFunctionSx);
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#E4F2AE',
+    },
+    secondary: {
+      main: '#E4F2AE',
+      button: '#10812D'
+    },
+    box: {
+      main: "#10812D",
+      secondary: "#E4F2AE"
+    }
+  },
+});
+
+
+const element = (
+  
+  <ThemeProvider theme={theme} >
+    <Box width="auto" height="auto" backgroundColor="primary.main" >
+      <AppBar position="static"> 
+        <Box width="auto" height="auto"  sx={{ display: 'flex' }}>
+          
+          <Div  sx={{m:"auto", color:"#000000", fontSize:20}}>
+            <h1 >Plateforme de chimie</h1>
+          </Div>
+          <Div sx={{ml:"auto", mr:3, display: 'flex'}}>
+            {/**
+             * <ListingCours sx={{ml:3, mr:4, my:2, py:2, bgcolor:"secondary.button", fontSize:12}}/>
+             * <MonCompteMenu sx={{ml:3, mr:4, my:2, py:2, bgcolor:"secondary.button", fontSize:12}}></MonCompteMenu>
+             * 
+             */}
+            
+            
+          </Div>
+          
+        </Box>
+      </AppBar>
+     
+    <Box width="auto" height="auto" backgroundColor="secondary.main" sx={{ display: 'flex' }}>
+    </Box>
+      <div id="main" >
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>}> </Route>
+                <Route path="/cv" element={<Cv/>}> </Route>
+                <Route path="/portfolio" element={<Portfolio/>}> </Route>
+            </Routes>
+        </Router>
+      </div>
+    </Box>
+  </ThemeProvider>
+  
+
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <MenuBar/>
-  </React.StrictMode>
+  element
 );
 
 // If you want to start measuring performance in your app, pass a function
